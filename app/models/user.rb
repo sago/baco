509b9de5e555625d3beeb4a39d2cadccc2f13cb4
Saúl Gómez <ing.saul.gomez18@gmail.com>
2013-10-27 16:13:27 -0600

    Setting email validation_format_of

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
-  #validates_format_of :email, :with =>
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
   validates_uniqueness_of :email
 
   def self.authenticate(email, password)
